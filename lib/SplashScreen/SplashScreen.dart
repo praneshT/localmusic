47999d88f33f4c3886c392574b3d78fd25dfeae9
praneshT <praneshT.zigainfotech@gmail.com>
2024-05-03 17:04:54 +0530

    login

--- lib/SplashScreen/SplashScreen.dart
+++ lib/SplashScreen/SplashScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:localmusic/screen/OnbpardScreen/onboard1.dart';
 
 class SplashScreen extends StatefulWidget {
